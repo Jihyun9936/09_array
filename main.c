37403b52aaa37106b157ac717cb92df639ccebcd
Jihyun9936 <55350908+Jihyun9936@users.noreply.github.com>
2019-10-31 15:39:33 +0900

    Lab03

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+/*Lab01
+nt main(int argc, char *argv[]) {
 	int grade[5];
 	int i;
 	
@@ -18,5 +19,20 @@ int main(int argc, char *argv[]) {
 	
 	
 	return 0;
+}*/
+
+/*Lab03*/
+int main(){
+	int i;
+	int grade[5];
+	int score[5];
+	
+	for(i=0;i<5;i++){
+		grade[i] = rand()%100;
+		score[i] = grade[i];
+	}
+	for(i=0;i<5;i++)
+		printf("score[%d] = %d\n", i, score[i]);
+		
 	return 0;
 }
